6d6e26a628fa10bfa7acb9fc02be62bb651a5c98
vivekojha85 <vivekojha85@gmail.com>
2019-01-17 18:16:40 -0500

    Changed

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY ch.py ./
 
+CMD python ./ch.py
--- ch.py
+++ ch.py
@@ -1,11 +1,4 @@
-# This program performs addition of two numbers
 
-num1 = input('What is your first value?\n')
-num2 = input('What is your second value?\n')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The ADDITION  of {0} and {1} is {2}'.format(num1, num2, sum))
+# Prints the name of your favourite TV series
+print('Game of thrones')
 
