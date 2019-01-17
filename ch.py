dd349ccf2f4d81aa3f83df1f55ef8a0208a1222d
vivekojha85 <vivekojha85@gmail.com>
2019-01-17 18:07:06 -0500

    Initial Commit

---
+++ ch.py
@@ -0,0 +1,11 @@
+# This program performs addition of two numbers
+
+num1 = input('What is your first value?\n')
+num2 = input('What is your second value?\n')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The ADDITION  of {0} and {1} is {2}'.format(num1, num2, sum))
+
